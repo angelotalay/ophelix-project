de84983be48501e5c5d1726ddd738a5ea7db12ff
Angelo Talay <angelogabrieltalay@gmail.com>
2025-10-20 15:09:45 +0100

    Additional set up files with Sanity headless CMS.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
-import Nav from "@/components/nav/Nav";
+import Nav from "@/components/common/nav/Nav"
+import Landing from "@/components/home/Landing"
 
 export default function Home() {
-  return (
-      <main className="light">
-          <Nav/>
-      </main>
-  );
+    return (
+        <main className="light">
+            <Landing/>
+        </main>
+    );
 }
--- src/components/common/nav/Nav.tsx
+++ src/components/common/nav/Nav.tsx
@@ -16,20 +16,32 @@ import {
 import navMenuItems from "@/config/home/navMenu";
 
 // Icons
-function UserIcon(){
-    return(
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+function UserIcon() {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"
+             className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round"
+                  d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"/>
         </svg>
 
     )
+};
+
+function UserButton() {
+    return (
+        <div>
+            <Button isIconOnly aria-label="User" color="secondary" variant="light" size="lg">
+                <UserIcon/>
+            </Button>
+        </div>
+    )
 }
 
-export default function Nav(){
+export default function Nav() {
     const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
-    return(
-        <Navbar onMenuOpenChange={setIsMenuOpen} className="bg-primary flex justify-between">
+    return (
+        <Navbar onMenuOpenChange={setIsMenuOpen} className="bg-primary" maxWidth="full">
             <NavbarContent justify="start" className="items-center">
                 <NavbarMenuToggle
                     aria-label={isMenuOpen ? "Close menu" : "Open menu"}
@@ -40,29 +52,12 @@ export default function Nav(){
             </NavbarContent>
             <NavbarContent className="hidden sm:flex gap-4" justify="center">
                 <NavbarItem>
-                    <Link color="foreground" href="#">
-                        Features
-                    </Link>
-                </NavbarItem>
-                <NavbarItem isActive>
-                    <Link aria-current="page" href="#">
-                        Customers
-                    </Link>
-                </NavbarItem>
-                <NavbarItem>
-                    <Link color="foreground" href="#">
-                        Integrations
-                    </Link>
+                    LOGO
                 </NavbarItem>
             </NavbarContent>
             <NavbarContent justify="end">
-                <NavbarItem className="hidden lg:flex">
-                    <Link href="#">Login</Link>
-                </NavbarItem>
                 <NavbarItem>
-                    <Button as={Link}  color="primary" href="#" variant="flat">
-                        Sign Up
-                    </Button>
+                    <UserButton/>
                 </NavbarItem>
             </NavbarContent>
             <NavbarMenu>
@@ -88,6 +83,8 @@ export default function Nav(){
                 </div>
             </NavbarMenu>
         </Navbar>
+
+
     )
 };
 
